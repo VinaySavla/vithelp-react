4587ee4fa72db39b2cac2fc058f0258e8426f9a1
harryme17 <89120880+harryme17@users.noreply.github.com>
2021-08-19 13:23:28 +0530

    Add files via upload
    Selection page uploaded

---
+++ src/pages/SelectionPage.tsx
@@ -0,0 +1,42 @@
+import { IonButton, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar,IonRippleEffect } from '@ionic/react';
+import { call, receipt, shield } from "ionicons/icons";
+import './selectionPage.css';
+
+const Selection: React.FC = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar color="danger">
+                    <IonTitle></IonTitle>
+                </IonToolbar>
+            </IonHeader>
+
+            <IonContent fullscreen>
+                <div id="container">
+                    <div>
+                        <img id="image" src="/assets/images/VIT-logo.png" alt="" />
+
+                    </div>
+
+
+                    <IonButton color="danger" size="large" fill="outline" expand="block" id="volunter-button" strong>
+                        <IonIcon slot="start" icon={shield} />
+                        Volunteer
+                    </IonButton>
+                    <IonButton color="danger" size="large" fill="outline" expand="block" id="distressed-button" strong>
+                        <IonIcon slot="start" icon={receipt} />
+                        Distressed
+                        <IonRippleEffect></IonRippleEffect> 
+                    </IonButton>
+
+                    <div id="button">
+                        <IonButton color="dark" expand="block">SIGN IN</IonButton>
+                    </div>
+                </div>
+            </IonContent>
+
+        </IonPage>
+    );
+};
+
+export default Selection;
